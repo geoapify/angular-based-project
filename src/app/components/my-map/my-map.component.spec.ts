89cdccba87665a337a0149de364e778c022e1db7
Alfiya Tarasenko <alfiya.tarasenko@geoapify.com>
2021-04-09 14:59:26 +0200

    Update to angular 11

--- src/app/components/my-map/my-map.component.spec.ts
+++ src/app/components/my-map/my-map.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MyMapComponent } from './my-map.component';
 
@@ -6,7 +6,7 @@ describe('MyMapComponent', () => {
   let component: MyMapComponent;
   let fixture: ComponentFixture<MyMapComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MyMapComponent ]
     })
